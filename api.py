6b1de81a0fab4280f22d5ebb0da3c651e1ba21b1
Tarcila Garcia <tarcilasg@gmail.com>
2022-08-19 00:26:27 -0300

    cleaning some comments and maintain the pattern on try/except/finally

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-import json
 from distutils.log import debug
 
 from flask import Flask, Response, jsonify
@@ -29,10 +28,9 @@ def get_stored_animals():
     db = ""
     try:
         db = get_db()
-        _animals = list(db.animal_tb.find())
+        _animals = db.animal_tb.find()
         animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
         return jsonify({"animal": animals})
-        #return Response(jsonify(**_animals))
     except:
         return jsonify({"message": "your request doesn't return any animal."})
     finally:
@@ -44,9 +42,13 @@ def get_wild_animals():
     try:
         db = get_db()
         _animals = db.animal_tb.find({type: "Wild"})
-        return Response(jsonify(**_animals))
+        animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
+        return jsonify({"animal": animals})
     except:
-        return jsonify({"error": "there's no one wild animal registered."})
+        return jsonify({"message": "there's no one wild animal registered."})
+    finally:
+        if type(db)==MongoClient:
+            db.close()
 
 if __name__=='__main__':
     api.run(host="0.0.0.0", port=5000, debug=True)
