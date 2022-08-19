5bc827cfbf6362cfd8d21992d3cf259214106c5c
Tarcila Garcia <tarcilasg@gmail.com>
2022-08-18 21:38:25 -0300

    update return on main route and other logics

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
-
+import json
 from distutils.log import debug
-from flask import Flask, jsonify
+
+from flask import Flask, Response, jsonify
 from pymongo import MongoClient
 
+#mongodb://root:pass@test_mongodb:27017
 # Initiate Flask api
 api = Flask(__name__)
 
@@ -20,21 +22,31 @@ def get_db():
 # Routes
 @api.route('/')
 def ping_server():
-    return "Welcome to the world of Mongo!"
+    return jsonify({"welcome": "Welcome to the world of Mongo!"})
 
 @api.route('/animals')
 def get_stored_animals():
-    db=""
+    db = ""
     try:
         db = get_db()
-        _animals = db.animal_tb.find()
+        _animals = list(db.animal_tb.find())
         animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
-        return jsonify({"animals": animals})
+        return jsonify({"animal": animals})
+        #return Response(jsonify(**_animals))
     except:
-        pass
+        return jsonify({"message": "your request doesn't return any animal."})
     finally:
         if type(db)==MongoClient:
             db.close()
+
+@api.route('/animals/wild')
+def get_wild_animals():
+    try:
+        db = get_db()
+        _animals = db.animal_tb.find({type: "Wild"})
+        return Response(jsonify(**_animals))
+    except:
+        return jsonify({"error": "there's no one wild animal registered."})
 
 if __name__=='__main__':
     api.run(host="0.0.0.0", port=5000, debug=True)
--- init-db.js
+++ init-db.js
@@ -1,20 +1,35 @@
 // Runs only after Database creation: populates database
-
-db = db.getSiblingDB('animal_db')
+//db aqui é o construído pelo MongoDB
+//db = db["animal_db"];
+db = db.getSiblingDB("animal_db");
 
 // Drop Existing Animal "Table"
-db.animal_tb.drop()
+db.animal_tb.drop();
 
 // Recreate Animal "Table"
-db.animal_tb.insertMany([
-    {
-        "id":1,
-        "name":"Lion",
-        "type":"Wild"
-    },
-    {
-        "id":2,
-        "name":"Cow",
-        "type":"Domestic"
-    }
+//cria a coleção
+db.animal_tb.insert_many([
+  {
+    id: 1,
+    name: "Lion",
+    type: "Wild",
+  },
+  {
+    id: 2,
+    name: "Cow",
+    type: "Domestic",
+  },
 ]);
+
+/* db.animal_tb = ([
+  {
+    id: 1,
+    name: "Lion",
+    type: "Wild",
+  },
+  {
+    id: 2,
+    name: "Cow",
+    type: "Domestic",
+  },
+]); */
